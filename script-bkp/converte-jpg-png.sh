ae9ce5c290dc22466829ae97fdb28808a771b66f
jonathan <jonathanrib.3@gmail.com>
2021-09-20 12:26:39 -0300

    ajustes do repo e script da aula 2

---
+++ script-bkp/converte-jpg-png.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+converte-imagem(){
+cd ~/workspace/linux/imagens-livros
+
+if [ ! -d png ]
+then
+	    mkdir png
+fi
+for imagem in *.jpg
+do
+	local img_sem_extensao=$(ls $imagem | awk -F . '{ print $1 }')
+	convert $img_sem_extensao.jpg png/$img_sem_extensao.png
+done
+}
+converte-imagem 2>erros_conversao.txt
+
+if [ $? -eq 0 ]
+then
+	echo "Conversao realizada com sucesso"
+else
+	echo "Houve uma falha no processo"
+fi
